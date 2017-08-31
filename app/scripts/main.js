46ae19bab5e7f9a6ab9a2444dcc851260fdf7a1a
James Sun <jamessun@thegoodguys.com.au>
2017-09-01 09:40:38 +1000

    Commit last changes for 'ggds-core.css'

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
     $('#auroraBody').load('aurora-resource/aurora-body.html');
     $('#newMasthead').load('refactor/masthead.html');
+    function loadSlashingPrice() {
+    	$('#slashingPrice').load('aurora-resource/slashing-price.html');
+	}
+	setTimeout(loadSlashingPrice, 1000);
 });
